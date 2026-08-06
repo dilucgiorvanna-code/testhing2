120e6c939ea3993cf9865c1111a874b43c2f77ea
dilucgiorvanna-code <dilucgiorvanna@gmail.com>
2026-08-05 21:39:13 -0300

    Add files via upload

---
+++ maps/vrosaremake.gd
@@ -0,0 +1,28 @@
+@phase1anims
+
+
+idle
+attack2
+die 
+hit
+attack
+upwards_25percent
+
+@phase2anims
+
+phase 2
+p2heavy_2
+p2jump
+p2die
+p2heavy
+P2attack
+staredownp2
+phase2idle
+
+hp 100
+
+walk speed 100
+terror radius 100
+damage is different per each attack.
+
+show me how to calculate and time htiboxes to attacks
